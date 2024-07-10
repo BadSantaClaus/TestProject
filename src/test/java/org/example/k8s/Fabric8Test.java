b62a08512a19face5db2220ee4ef36ab42456ace
Горячев Роман Юрьевич (4110100) <rugoryachev@vtb.ru>
2024-07-10 17:45:50 +0300

    Add k8s read log test

--- src/test/java/org/example/k8s/Fabric8Test.java
+++ src/test/java/org/example/k8s/Fabric8Test.java
@@ -1,5 +1,6 @@
 package org.example.k8s;
 
+import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.apps.DeploymentSpec;
 import io.fabric8.kubernetes.client.Config;
@@ -11,6 +12,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class Fabric8Test {
@@ -20,17 +23,40 @@ public class Fabric8Test {
         String namespace = "default";
         String deploymentName = "simple-nginx-588f84876f-f4clx";
         int newReplicaCount = 2;
+        String containerName = "nginx";
 
         String kubeconfigContents = Files.readString(Path.of("src/main/resources/config.yaml"));
         Config config = Config.fromKubeconfig(kubeconfigContents);
 
-        try (KubernetesClient client = new KubernetesClientBuilder().withConfig(config).build()) {
+//        try (KubernetesClient client = new KubernetesClientBuilder().withConfig(config).build()) {
+        try (KubernetesClient client = new KubernetesClientBuilder().build()) {
             Deployment deployment = client.apps().deployments().inNamespace(namespace).withName(deploymentName).get();
             DeploymentSpec spec = deployment.getSpec();
             spec.setReplicas(newReplicaCount);
 
             client.apps().deployments().inNamespace(namespace).withName(deploymentName).patch(deployment);
             System.out.println("Successfully scaled the deployment to " + newReplicaCount + " replicas.");
+
+            Map<String, String> podLogs = new HashMap<>();
+            PodList podList = client.pods().inNamespace(namespace).withLabel("app", deploymentName).list();
+
+            if (podList.getItems().isEmpty())
+                log.info("Поды, связанные с деплойментом {}, не найдены.", deploymentName);
+
+            podList.getItems().forEach(pod -> {
+                String podName = pod.getMetadata().getName();
+                log.info("Попытка вычитать логи из контейнера '{}' пода '{}'", containerName, podName);
+                String logs = client.pods()
+                        .inNamespace(namespace)
+                        .withName(podName)
+                        .inContainer(containerName)
+                        .getLog();
+                podLogs.put(podName, logs);
+
+                log.info("logs сontains: {}", logs.contains("1048576"));
+                podLogs.forEach((key, value) -> log.info("podLogs сontains: {}", value.contains("1048576")));
+            });
+
         }
     }
 }
